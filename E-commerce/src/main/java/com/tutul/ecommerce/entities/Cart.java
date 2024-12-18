6a3976460ffd083e65d36b75c80b99c9bb8aa798
faiaz <ahsan@newroztech.com>
2024-12-18 19:02:01 +0600

    optimize cart entity

--- E-commerce/src/main/java/com/tutul/ecommerce/entities/Cart.java
+++ E-commerce/src/main/java/com/tutul/ecommerce/entities/Cart.java
@@ -16,39 +16,8 @@ public class Cart {
     @ManyToOne
     private Product product;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
     private Integer quantity;
+
     private Double totalPrice;
 
 }
